0a6cc60cd6e431431ba3c7c563f1f1bf9bfa679b
mertaytuggok <mertaytuggok@gmail.com>
2024-10-02 15:55:20 +0300

    I'm back

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,10 @@
 import React from "react";
-import styles from "./Footer.module.css";
 import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className={styles.mainFooter}>
-      <span className={styles.footerName}>Diyetisyen Yunus Aksu</span>{" "}
+    <div className=" flex flex-col items-center justify-center gap-8 bg-[#f8f4f1] py-20 text-center text-xl sm:px-4 sm:py-8">
+      <span className="font-semibold">Diyetisyen Yunus Aksu</span>{" "}
       <Link href={"mailto:dyt.yunusaksu@gmail.com"}>
         dyt.yunusaksu@gmail.com
       </Link>{" "}
@@ -13,6 +12,12 @@ const Footer = () => {
       <span>
         {" "}
         Kızılcaşar Mahallesi Mehmetçik Caddesi No:21 İncek/Gölbaşı/Ankara
+      </span>
+      <span className=" flex flex-col">
+        <span className="text-sm font-medium">
+          Telif Hakkı © 2024 YUNUS AKSU
+        </span>
+        <span className="text-sm font-medium">Tüm Hakları Saklıdır.</span>
       </span>
     </div>
   );
--- src/app/About/page.js
+++ src/app/About/page.js
@@ -1,5 +1,51 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="flex w-full items-center justify-center py-12">
+      <div className="grid w-[45%] items-center justify-center  gap-8 text-center">
+        <div>Fotoğraf</div>
+        <div className=" grid items-center justify-between gap-8 rounded-md bg-rose-100  py-8">
+          <span className="text-xl font-bold">
+            Merhaba, ben Diyetisyen Yunus Aksu
+          </span>
+          <span className="text-lg font-medium">
+            Hayalimdeki mesleğe Hacettepe Üniversitesinde Beslenme ve Diyetetik
+            bölümünü şeref öğrencisi olarak tamamlayarak ulaştım.
+          </span>
+          <span className="text-lg font-medium">
+            Üniversite eğitimim süresince Canan Aksoy Beslenme Danışmanlığı
+            Merkezi, Hacettepe Üniversitesi Erişkin Hastanesi, Hacettepe
+            Üniversitesi Onkoloji Hastanes, ve İhsan Doğramacı Çocuk
+            Hastanesi’nde yetişkin ve çocuk beslenmesi üzerine, Ankara Şehir
+            Hastanesi ve Hacettepe Üniversitesi Kafeteryaları mutfaklarında
+            kurum beslenmesi üzerine stajlar yaparak kendimi mesleğime
+            hazırladım.
+          </span>
+          <span className="text-lg font-medium">
+            Lisans eğitimimin son yılında Müziğin Varlığı, Müzik Türü ve
+            Şiddetinin Akut Besin Tüketimine Etkisi konulu bir tez ve Sirkadiyen
+            Ritim ve Beslenme konulu bir seminer hazırladım.
+          </span>
+          <span className="text-lg font-medium">
+            Sporcu Beslenmesi, Bariatrik Cerrahi, Obezite ve Diyabet Tedavisi
+            gibi konular başta olmak üzere birçok eğitim ve kurslara katıldım.
+            Hala da bilgilerimi güncel tutabilmek adına katılımlara devam
+            ediyorum.
+          </span>
+          <span className="text-lg font-medium">
+            Meslek hayatıma bir spor kompleksinde beslenme danışmanlığı hizmeti
+            vererek başladım, bir güzellik merkezinde ve bir diyet kliniğinde de
+            hizmet verdikten sonra kendi beslenme danışmanlığı merkezimi kurdum.
+            Mesleğime kendi markam adı altında devam ediyorum.
+          </span>
+          <span className="text-lg font-medium">
+            Mesleğime başladığımdan beri amacım; birçok insanın hayatına
+            dokunarak onlara sağlıklı beslenmeyi öğretmek. Bugüne dek yüzlerce
+            danışanımla bunu başardık ve başarmaya devam ediyoruz.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
