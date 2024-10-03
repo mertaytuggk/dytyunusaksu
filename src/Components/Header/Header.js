bf57946843453bec5aa40e2ceb5365a5a7727878
mertaytuggok <mertaytuggok@gmail.com>
2024-10-03 14:36:51 +0300

    Bug Fix Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,26 +13,34 @@ const Header = () => {
   return (
     <div>
       {/* MOBİLE MENU BAŞLANGICI*/}
-      <div className={styles.mainMobile}>
+      <div
+        className={` relative flex items-center justify-between bg-[#f0f0f0] p-8 ${styles.mainMobile}`}
+      >
         <div>
-          <Link href={"/"} className={styles.headerLogoMobil}>
-            <Image className={styles.mobilLogo} src={Logo} alt="NavbarLogo" />
+          <Link href={"/"} className="items-left flex flex-col">
+            <Image className="w-[35%]" src={Logo} alt="NavbarLogo" />
 
-            <span className={styles.mobilSaksu}>Diyetisyen Yunus Aksu</span>
+            <span className="text-base">Diyetisyen Yunus Aksu</span>
           </Link>
         </div>
-        <button onClick={() => setOpen(!open)} className={styles.mobilIcon}>
+        <button onClick={() => setOpen(!open)}>
           <FaBars size={24} />
         </button>
       </div>{" "}
       {open ? (
-        <div className={styles.openMenu}>
-          <button onClick={() => setOpen(!open)} className={styles.menuClose}>
+        <div className=" absolute top-0 h-[100dvh] w-[100dvw] bg-[#f5c28b] px-12 py-16 ">
+          <button
+            onClick={() => setOpen(!open)}
+            className="absolute right-8 top-6 rotate-45"
+          >
             <FaPlus size={24} />
           </button>
-          <div className={styles.gridMenu}>
+          <div
+            onClick={() => setOpen(!open)}
+            className="items-left flex flex-col gap-6"
+          >
             <Link href={"/"}>Ana Sayfa</Link>
-            <Link href={"/about"}>Beni Tanıyın</Link>
+            <Link href={"/About"}>Beni Tanıyın</Link>
             <Link href={"/Services"}>Hizmetler</Link>
             <Link href={"/Blog"}>Blog</Link>
             <Link href={"/Contact"}>İletişim</Link>
@@ -57,18 +65,27 @@ const Header = () => {
       )}{" "}
       {/* MOBİLE MENU SONU*/}
       {/* WEB MENU BAŞLANGICI*/}
-      <div className={styles.mainHeader}>
-        <Link href={"/"} className={styles.headerLogo}>
-          <Image className={styles.Logo} src={Logo} alt="NavbarLogo" />
+      <div
+        className={`flex items-center justify-between bg-[#f8f4f1] p-5 shadow-lg ${styles.mainHeader}`}
+      >
+        <Link
+          href={"/"}
+          className="flex flex-col items-center justify-center font-semibold"
+        >
+          <Image className="h-1/2 w-1/2" src={Logo} alt="NavbarLogo" />
 
-          <span className={styles.Saksu}>Diyetisyen Yunus Aksu</span>
+          <span className="text-base">Diyetisyen Yunus Aksu</span>
         </Link>
-        <Link href={"/"} target="_blank" className={styles.randevuButton}>
+        <Link
+          href={"/"}
+          target="_blank"
+          className="rounded-[0.2rem] bg-[#a34a21] px-6 py-3 text-[1.1rem] font-medium text-white hover:bg-[#c15a31] "
+        >
           Randevu Al
         </Link>
       </div>
-      <div className={styles.upBarMain}>
-        <div className={styles.upBar}>
+      <div className={` ${styles.upBarMain}`}>
+        <div className={` shadow-lg ${styles.upBar}`}>
           <span className={styles.menuUpBar}>
             <Link href={"/"}>Ana Sayfa</Link>
             <Link href={"/About"}>Beni Tanıyın</Link>
@@ -95,7 +112,6 @@ const Header = () => {
             >
               <FaInstagram size={20} />
             </Link>
-            <button></button>
           </span>
         </div>
       </div>
