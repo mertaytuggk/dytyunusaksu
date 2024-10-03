1746391265da7b321cc47711837673afe4b7a8f6
mertaytuggok <mertaytuggok@gmail.com>
2024-10-03 14:37:17 +0300

    Bug Fix Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,17 +3,16 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className=" flex flex-col items-center justify-center gap-8 bg-[#f8f4f1] py-20 text-center text-xl sm:px-4 sm:py-8">
-      <span className="font-semibold">Diyetisyen Yunus Aksu</span>{" "}
+    <div className="flex flex-col items-center justify-center gap-8 bg-[#f8f4f1] py-20 text-center text-xl sm:px-4 sm:py-8 ">
+      <span className="font-semibold">Diyetisyen Yunus Aksu</span>
       <Link href={"mailto:dyt.yunusaksu@gmail.com"}>
         dyt.yunusaksu@gmail.com
-      </Link>{" "}
+      </Link>
       <Link href={"tel:+90 534 544 05 06"}>+90 534 544 05 06</Link>
       <span>
-        {" "}
         Kızılcaşar Mahallesi Mehmetçik Caddesi No:21 İncek/Gölbaşı/Ankara
       </span>
-      <span className=" flex flex-col">
+      <span className="flex flex-col">
         <span className="text-sm font-medium">
           Telif Hakkı © 2024 YUNUS AKSU
         </span>
